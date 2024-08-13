e84b0c87d8b83220becaceb99761d5a4edc3f444
hendisantika <hendisantika@yahoo.co.id>
2024-08-13 15:19:02 +0700

    Add Protect auth service🫸🌀✏️📗🐧🐳

--- src/services/authservice.go
+++ src/services/authservice.go
@@ -1,8 +1,12 @@
 package services
 
 import (
+	"encoding/json"
 	"github.com/Nerzal/gocloak/v13"
+	"go-keycloak-sample/src/errors"
+	"net/http"
 	"os"
+	"strings"
 )
 
 type LoginResponse struct {
@@ -22,4 +26,38 @@ var client gocloak.GoCloak
 
 func InitializeOauthServer() {
 	client = gocloak.NewClient(hostname)
+}
+
+func Protect(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		authHeader := r.Header.Get("Authorization")
+
+		if len(authHeader) < 1 {
+			w.WriteHeader(401)
+			json.NewEncoder(w).Encode(errors.UnauthorizedError())
+			return
+		}
+
+		accessToken := strings.Split(authHeader, " ")[1]
+
+		rptResult, err := client.RetrospectToken(r.Context(), accessToken, clientId, clientSecret, realm)
+
+		if err != nil {
+			w.WriteHeader(400)
+			json.NewEncoder(w).Encode(errors.BadRequestError(err.Error()))
+			return
+		}
+
+		isTokenValid := *rptResult.Active
+
+		if !isTokenValid {
+			w.WriteHeader(401)
+			json.NewEncoder(w).Encode(errors.UnauthorizedError())
+			return
+		}
+
+		// Our middleware logic goes here...
+		next.ServeHTTP(w, r)
+	})
 }
