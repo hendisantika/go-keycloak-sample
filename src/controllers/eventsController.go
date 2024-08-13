6a8ec0138faa259351ad7f5b6817a3535172900a
hendisantika <hendisantika@yahoo.co.id>
2024-08-13 15:20:41 +0700

    Add RegisterRoutes in Controller🫸🌀✏️📗🐧🐳

--- src/controllers/eventsController.go
+++ src/controllers/eventsController.go
@@ -1,4 +1,16 @@
 package controllers
 
+import (
+	"github.com/gorilla/mux"
+	"go-keycloak-sample/src/services"
+	"net/http"
+)
+
 type EventController struct {
+}
+
+func (t EventController) RegisterRoutes(router *mux.Router) {
+	router.Handle("/event", services.Protect(http.HandlerFunc(services.CreateEvent))).Methods("POST")
+	router.Handle("/events/{id}", services.Protect(http.HandlerFunc(services.GetOneEvent))).Methods("GET")
+	router.Handle("/events", services.Protect(http.HandlerFunc(services.AllEvents))).Methods("GET")
 }
