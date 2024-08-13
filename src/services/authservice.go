fdf2063b8776985fe2975bba85bc4a67527faef0
hendisantika <hendisantika@yahoo.co.id>
2024-08-13 15:16:46 +0700

    Add ENV Vars🫸🌀✏️📗🐧🐳

--- src/services/authservice.go
+++ src/services/authservice.go
@@ -1,7 +1,16 @@
 package services
 
+import "os"
+
 type LoginResponse struct {
 	AccessToken string `json:"access_token"`
 	Title       string `json:"Title"`
 	Description string `json:"Description"`
 }
+
+var (
+	clientId     = os.Getenv("CLIENT_ID")
+	clientSecret = os.Getenv("CLIENT_SECRET")
+	realm        = os.Getenv("REALM")
+	hostname     = os.Getenv("HOST")
+)
