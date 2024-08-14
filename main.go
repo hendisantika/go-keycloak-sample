7e0f6d0dd1e421af69b4c640caefec3370b282e1
hendisantika <hendisantika@yahoo.co.id>
2024-08-14 15:54:06 +0700

    feat: Add registerControllerRoutes function🫸🌀✏️📗🐧🐳

--- main.go
+++ main.go
@@ -23,3 +23,7 @@ func run() {
 func registerRoutes(router *mux.Router) {
 	registerControllerRoutes(controllers.EventController{}, router)
 }
+
+func registerControllerRoutes(controller controllers.Controller, router *mux.Router) {
+	controller.RegisterRoutes(router)
+}
